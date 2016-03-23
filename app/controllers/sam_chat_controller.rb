bd03f446c7fbebe950dcf20b155e79df47441c18
Sam Davies <seivadmas@gmail.com>
2016-03-23 16:31:53 -0300

    Add api controller actions

--- app/controllers/sam_chat_controller.rb
+++ app/controllers/sam_chat_controller.rb
@@ -4,4 +4,19 @@ class SamChatController < ApplicationController
       messages: Message.all.as_json
     }
   end
+
+  def api_list_messages
+    render json: { messages: Message.all.as_json }
+  end
+
+  def api_post_message
+    m = Message.create!(message_params)
+    render json: { message: { id: m.id } }, status: :created
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:author, :body, :send_at)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
-  get 'samchat', to: 'sam_chat#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # Serve websocket cable requests in-process
+  scope constraints: { format: :html } do
+    get 'samchat', to: 'sam_chat#index'
+  end
+
+  scope constraints: { format: :json } do
+    get 'api/messages', to: 'sam_chat#api_list_messages'
+    post 'api/message', to: 'sam_chat#api_post_message'
+  end
+
   # mount ActionCable.server => '/cable'
 end
