6ff2763550730083e446eecfae8a41cda2226d7d
Sam Davies <seivadmas@gmail.com>
2016-03-25 15:12:10 -0300

    Enforce invariant state

--- client/app/bundles/SamChat/store/applyMiddleware.dev.js
+++ client/app/bundles/SamChat/store/applyMiddleware.dev.js
@@ -2,9 +2,10 @@ import DevTools from '../containers/DevTools';
 import thunk from 'redux-thunk';
 import { applyMiddleware, compose } from 'redux';
 import createLogger from 'redux-logger';
+import stateInvariant from 'redux-immutable-state-invariant';
 
 export default compose(
   // development middleware
-  applyMiddleware(thunk, createLogger()),
+  applyMiddleware(stateInvariant(), thunk, createLogger()),
   DevTools.instrument(),
 );
