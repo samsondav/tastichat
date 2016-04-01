f589feb6e0978848b5c1eb83c6d2e22da6fa6f4e
Sam Davies <seivadmas@gmail.com>
2016-04-02 00:35:40 +0800

    Dont attempt to load devtools in production

--- client/app/bundles/Tastichat/startup/TastichatAppClient.jsx
+++ client/app/bundles/Tastichat/startup/TastichatAppClient.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import RootContainer from '../containers/RootContainer';
 import createStoreFromProps from '../store/applicationStore';
-import DevTools from '../containers/DevTools';
 import Config from 'lib/Config';
 
 // props.config is special, anything in here gets assigned to the global Config
@@ -18,6 +17,5 @@ export default props => {
   setConfigFromProps(props);
   return <Provider store={createStoreFromProps(props)}>
     <RootContainer />
-    {/*<DevTools />*/}
   </Provider>
 };
